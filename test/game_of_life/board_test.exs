b2b5012aeb1700849709652e2032939374168fcb
Josh Adams <josh@isotope11.com>
2014-12-07 20:10:53 -0600

    Start board test...

--- test/game_of_life/board_test.exs
+++ test/game_of_life/board_test.exs
@@ -1,8 +1,8 @@
 defmodule GameOfLife.BoardTest do
   use ExUnit.Case
 
-  """
   test "creates a child process for each cell in the initial template" do
+    board = Board.start_link(template)
+    assert 8 == Supervisor.count_children(board)
   end
-  """
 end
